31464d3274147d23c09241c883748a28ccea6e89
Marouano <marouan.bakour96@gmail.com>
2016-04-07 14:29:48 +0200

    add speler class

---
+++ JCF Opdracht 3/src/jcf/opdracht/pkg3/Speler.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jcf.opdracht.pkg3;
+
+/**
+ *
+ * @author marouano
+ */
+public class Speler {
+    public String userName;
+    public
+}
